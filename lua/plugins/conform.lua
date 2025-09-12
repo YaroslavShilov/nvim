cb99d1b40ec92c2b9ed07fbcb4e4f1280211330c
Yaroslav Shilov <workyarik@gmail.com>
2025-09-12 17:29:25 +0400

    Change logo

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      markdown = { "prettier" },
+    },
+  },
+}
